c6e617bf98cd041a2f48e0668b6d6f590af8f24b
Sylvia Roselie <rose@rosely.me>
2025-01-20 19:14:08 -0500

    Add Zsh configuration files for command-not-found, completions, history, theme, and zinit

---
+++ dotfiles/dot_zsh/zinit.zsh
@@ -0,0 +1,75 @@
+ZINIT_HOME="${HOME}/.zinit"
+
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
+
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+
+
+
+#################################################################
+# FUNCTIONS TO MAKE CONFIGURATION LESS VERBOSE
+#
+
+turbo0()   { zinit ice wait"0a" lucid             "${@}"; }
+turbo1()   { zinit ice wait"0b" lucid             "${@}"; }
+turbo2()   { zinit ice wait"0c" lucid             "${@}"; }
+zcommand() { zinit ice wait"0b" lucid as"command" "${@}"; }
+zload()    { zinit load                           "${@}"; }
+zsnippet() { zinit snippet                        "${@}"; }
+
+# Print command exit code as a human-readable string
+zload bric3/nice-exit-code
+export RPS1='%B%F{red}$(nice_exit_code)%f%b'
+
+# Install `fzf` bynary and tmux helper script
+zcommand from"gh-r";         zload junegunn/fzf-bin
+zcommand pick"bin/fzf-tmux"; zload junegunn/fzf
+# Create and bind multiple widgets using fzf
+turbo0 multisrc"shell/{completion,key-bindings}.zsh" \
+        id-as"junegunn/fzf_completions" pick"/dev/null"
+    zload junegunn/fzf
+
+# Fuzzy movement and directory choosing
+turbo1; zload rupa/z               # autojump command
+turbo0; zload andrewferrier/fzf-z  # Pick from most frecent folders with `Ctrl+g`
+turbo0; zload changyuheng/fz       # lets z+[Tab] and zz+[Tab]
+
+
+#################################################################
+# IMPORTANT PLUGINS
+#
+
+# Additional completion definitions
+turbo0 blockf
+zload zsh-users/zsh-completions
+
+# History search by `Ctrl+R`
+turbo1; zload zdharma/history-search-multi-word
+
+# Syntax highlighting
+# (compinit without `-i` spawns warning on `sudo -s`)
+turbo0 atinit"ZINIT[COMPINIT_OPTS]='-i' zpcompinit; zpcdreplay"
+    zload zdharma/fast-syntax-highlighting
+
+# Autosuggestions
+# Note: should go _after_ syntax highlighting plugin
+turbo0 atload"_zsh_autosuggest_start"; zload zsh-users/zsh-autosuggestions
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+
+# NVM plugin code:
+export NVM_LAZY_LOAD=true
+export NVM_AUTO_USE=true
+turbo0; zload lukechilds/zsh-nvm
+
+
+#################################################################
+# REMOVE TEMPORARY FUNCTIONS
+#
+unset -f turbo0
+unset -f zload
+unset -f zsnippet
